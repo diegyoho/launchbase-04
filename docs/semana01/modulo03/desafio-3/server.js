5e5a18769e4ae5c25dd37fee5dc745ed4a0874b6
Diego Moreira <chicodiegomoreira@gmail.com>
2020-04-11 13:44:09 -0300

    About dynamic

--- docs/semana01/modulo03/desafio-3/server.js
+++ docs/semana01/modulo03/desafio-3/server.js
@@ -8,11 +8,45 @@ server.set('view engine', 'njk')
 server.use(express.static('public'))
 
 nunjuks.configure('views', {
-    express: server
+    express: server,
+    autoescape: false
 })
 
 server.get('/', function (req, res) {
-    res.render('about')
+    const about = {
+        name: 'Rocketseat',
+        logo: 'https://rocketseat.com.br/static/images/logo-rocketseat.svg',
+        description: 'As melhores tecnologias em programação, direto ao ponto e do jeito certo.',
+        techs: [
+            {
+                name: 'NodeJS',
+                logo: 'https://nodejs.org/static/images/logo.svg'
+            },
+            {
+                name: 'ReactJS',
+                logo: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K'
+            },
+            {
+                name: 'React Native',
+                logo: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K'
+            }
+        ],
+        links: [
+            {
+                name: 'Github',
+                url: 'https://github.com/Rocketseat'
+            },
+            {
+                name: 'Instagram',
+                url: 'https://www.instagram.com/rocketseat_oficial/'
+            },
+            {
+                name: 'Facebook',
+                url: 'https://www.facebook.com/rocketseat'
+            }
+        ]
+    }
+    res.render('about', { about })
 })
 
 server.get('/courses', function (req, res) {
